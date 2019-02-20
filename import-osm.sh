dc15de6ad397faaecfd24b15a284b45244fd4eda
Danny Wolf <wolf@students.olin.edu>
2019-02-19 21:31:09 -0500

    *shakes fist at clouds*

--- import-osm.sh
+++ import-osm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [ -z "$DIRECTORY_OF_OSM_FILES" ]
 then
@@ -19,9 +20,9 @@ FREE_MEM=$(free -m | awk '/^Mem:/{print $4}')
 PROB_SAFE_MEM=$(($FREE_MEM / 6 * 4))
 
 docker run -it --rm \
-  --network mapping-stack_default \
+  --network local-mapping_default \
   -e PGPASS=secretmapping \
-  --link $(docker-compose ps -q postgis):pg \
+  --link $(docker-compose ps postgis | sed -n 3p | cut -d" " -f1):pg \
   -v $DIRECTORY_OF_OSM_FILES:/osm \
   -v "$(pwd)"/openstreetmap-carto:/transforms openfirmware/osm2pgsql \
   -c "osm2pgsql --create --slim --hstore --style /transforms/openstreetmap-carto.style --tag-transform-script /transforms/openstreetmap-carto.lua --cache $PROB_SAFE_MEM --number-processes $NUM_CORES --database osm --username osm --host pg --port 5432 /osm/$OSM_FILE"
